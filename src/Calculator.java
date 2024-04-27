986447ec3e71ff096c921d1995e9f1e868c930a0
radzag7127 <radzaglagah@gmail.com>
2024-04-27 15:40:39 +0700

    com 2

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,6 +14,10 @@ public class Calculator {
     public int multiply(int a, int b) {
         return a * b;
     }
+    public double dividebaru(int a, int b) {
+        return (double) a / b;
+    }
+
 
 
     public static void main(String[] args) {
@@ -22,5 +26,6 @@ public class Calculator {
         System.out.println("Penjumlahan 5 + 3 = " + tor.add(5, 3));
         System.out.println("Pengurangan 5 - 3 = " + tor.subtract(5, 3));
         System.out.println("Perkalian 5 * 3 = " + tor.multiply(5, 3));
+        System.out.println("Pembagian 5 / 3 = " + tor.dividebaru(5, 3));
     }
 }
